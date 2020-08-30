6c9d732c8da6ff632e4ab45255317ab26abf7778
thineskrishna <a.thineskrishna@gmail.com>
2020-08-30 15:53:59 +0100

    Updates

---
+++ component---src-pages-index-js-e7957c2b5d75f62cd001.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{RXBc:function(e,a,t){"use strict";t.r(a);var n=t("q1tI"),l=t.n(n),c=t("Bl7J");a.default=function(){return l.a.createElement("html",null,l.a.createElement("div",{class:"pageloader is-active"},l.a.createElement("span",{class:"title"},"Pageloader"),l.a.createElement(c.a,null)))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-e7957c2b5d75f62cd001.js.map
